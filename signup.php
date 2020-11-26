02d024278951ab205f2e4d846ce47d17807eb37d
naomichi <naomichi.programming@gmail.com>
2020-11-26 20:07:14 +0900

    最初のコミットです

---
+++ signup.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Idea Box | 会員登録</title>
+    <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body class="background-color">
+    <div class ="form-wrap">
+        <img class="site-icon" src="images/idea-box-icon.png" alt="idea-box-icon">
+        <h1 class ="title">Idea Box</h1>
+        <p class = "catch">アイデアから、アイデアを。</p>
+        <form action="" method="post">
+            <input class="form-input" type="text" name="emal" placeholder="Eメール">
+            <input class="form-input" type="text" name="password" placeholder="パスワード">
+            <input class="form-input" type="text" name="re_password" placeholder="パスワード再入力">
+            <input class="btn form-btn" type="submit" value="新規登録">
+        </form>
+        <div class="form-comment">
+        <p>アカウントを作成すると、サービス<a href="">利用規約</a>および<br>
+        <a href="">プライバシーポリシー</a>に同意したとみなされます。</p>
+        <p>すでにアカウントをお持ちですか？<br>
+        <a class="login" href="">ログイン</a></p>
+        </div>
+    </div>
+</body>
+</html>
