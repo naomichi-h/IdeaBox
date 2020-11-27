ea027add5fd4eb7c55c943725fab9a95c3dd40b1
naomichi <naomichi.programming@gmail.com>
2020-11-27 17:15:47 +0900

    フォームのスタイル整形

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IdeaBox | トップページ</title>
+    <link rel="stylesheet" type="text/css" href="https://unpkg.com/ress/dist/ress.min.css" >
+    <link rel="stylesheet" type="text/css" href="style.css" >
+</head>
+<body>
+    <nav class="top-nav">
+        <h1 class="site-title">IdeaBox</h1>
+        <div class="nav-wrap">
+          <p><a href="login.php">ログイン</a></p>
+          <p><a href="signup.php">ユーザー登録</a></p>
+        </div>
+    </nav>
+    <div class="hero">New idea is the old idea on the new position</div>
+    
+</body>
+</html>
